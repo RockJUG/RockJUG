e9ed8c89bed1ce74502173d008fd561321118e55
adamclmns <adam@adamclmns.com>
2018-10-23 21:29:20 -0500

    initial

---
+++ src/main/java/org/jug/littlerock/rockjug/RockjugApplication.java
@@ -0,0 +1,12 @@
+package org.jug.littlerock.rockjug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockjugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RockjugApplication.class, args);
+	}
+}
