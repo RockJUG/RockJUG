a3bfc7b8c419b58369c380dc2e8a79b295da900f
adamclmns <adam@adamclmns.com>
2018-10-25 06:51:59 -0500

    i broke it good. introducing light admin

--- src/main/java/org/jug/littlerock/rockjug/RockjugApplication.java
+++ src/main/java/org/jug/littlerock/rockjug/RockjugApplication.java
@@ -2,9 +2,31 @@ package org.jug.littlerock.rockjug;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import static org.lightadmin.core.util.LightAdminConfigurationUtils.LIGHT_ADMINISTRATION_BACK_TO_SITE_URL;
+import static org.lightadmin.core.util.LightAdminConfigurationUtils.LIGHT_ADMINISTRATION_BASE_PACKAGE;
+import static org.lightadmin.core.util.LightAdminConfigurationUtils.LIGHT_ADMINISTRATION_BASE_URL;
 
 @SpringBootApplication
-public class RockjugApplication {
+public class RockjugApplication extends SpringBootServletInitializer {
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		servletContext.setInitParameter(LIGHT_ADMINISTRATION_BASE_URL, "/admin");
+		servletContext.setInitParameter(LIGHT_ADMINISTRATION_BACK_TO_SITE_URL, "http://lightadmin.org");
+		servletContext.setInitParameter(LIGHT_ADMINISTRATION_BASE_PACKAGE, "org.lightadmin.administration");
+
+		super.onStartup(servletContext);
+	}
+	@Override
+	protected SpringApplicationBuilder configure(
+			SpringApplicationBuilder builder) {
+		return builder.sources(RockjugApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RockjugApplication.class, args);
