2368da7bc380c8e3e1a166a3a9950eaa4cf5a54f
adamclmns <adam@adamclmns.com>
2018-11-01 17:10:41 -0500

    removing lightadmin completely. site was taken over by malware, and project seems like it's gone dark.

--- src/main/java/org/jug/littlerock/rockjug/RockjugApplication.java
+++ src/main/java/org/jug/littlerock/rockjug/RockjugApplication.java
@@ -8,20 +8,10 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
-import static org.lightadmin.core.util.LightAdminConfigurationUtils.LIGHT_ADMINISTRATION_BACK_TO_SITE_URL;
-import static org.lightadmin.core.util.LightAdminConfigurationUtils.LIGHT_ADMINISTRATION_BASE_PACKAGE;
-import static org.lightadmin.core.util.LightAdminConfigurationUtils.LIGHT_ADMINISTRATION_BASE_URL;
 
 @SpringBootApplication
 public class RockjugApplication extends SpringBootServletInitializer {
-	@Override
-	public void onStartup(ServletContext servletContext) throws ServletException {
-		servletContext.setInitParameter(LIGHT_ADMINISTRATION_BASE_URL, "/admin");
-		servletContext.setInitParameter(LIGHT_ADMINISTRATION_BACK_TO_SITE_URL, "http://lightadmin.org");
-		servletContext.setInitParameter(LIGHT_ADMINISTRATION_BASE_PACKAGE, "org.lightadmin.administration");
 
-		super.onStartup(servletContext);
-	}
 	@Override
 	protected SpringApplicationBuilder configure(
 			SpringApplicationBuilder builder) {
