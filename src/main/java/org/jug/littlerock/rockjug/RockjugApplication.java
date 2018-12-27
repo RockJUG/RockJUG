516d9ebc7dacd307ef95f6ef8c1c5597b393ea5c
adamclmns <adam@adamclmns.com>
2018-12-27 12:16:06 -0600

    ready to post online. No features. Just a poster.

--- src/main/java/org/jug/littlerock/rockjug/RockjugApplication.java
+++ src/main/java/org/jug/littlerock/rockjug/RockjugApplication.java
@@ -5,9 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-
 
 @SpringBootApplication
 public class RockjugApplication extends SpringBootServletInitializer {
