eb3c99fa283fc3fc91046d19ef326961b9a4ebab
adamclmns <adam@adamclmns.com>
2018-12-27 11:27:45 -0600

    clean up. It runs locally. Will re-visit login features later.

---
+++ src/main/java/org/jug/littlerock/rockjug/interceptors/NavBarInterceptor.java
@@ -0,0 +1,10 @@
+package org.jug.littlerock.rockjug.interceptors;
+
+/**
+ * This Controller will handle all of the NavBar Content, and make it available on all responses
+ * This can also be configured to only run on certain paths.
+ * FUTURE TODO: Enabling caching of this data, so that it is not re-fetched for every request.
+ */
+public class NavBarInterceptor {
+
+}
